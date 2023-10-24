55aca5987eacf95d5fee60d3af0b9f35a8ece270
kynaios <blaise.witczak@gmail.com>
2023-10-24 08:41:10 +0200

    Register AutoMapper and CRUD repository and services inside builder

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
+using UserNotebook;
 using UserNotebook.Context;
-using UserNotebook.Types;
+using UserNotebook.Mappers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +13,25 @@ var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables()
     .Build();
+
 // Add services to the container.
+// AutoMapper
+var mapper = new MapperConfiguration(c =>
+{
+    c.AddProfile(new MapperProfile());
+}).CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+// builder.Services.AddTransient<ICrudRepository<Adult>, AdultRepository>();
+// builder.Services.AddTransient<ICrudService<AdultDto>, AdultService>();
+// builder.Services.AddTransient<ICrudRepository<Kid>, KidRepository>();
+// builder.Services.AddTransient<ICrudService<KidDto>, KidService>();
+
+builder.Services.AddScoped(typeof(ICrudRepository<>), typeof(CrudRepository<>));
+builder.Services.AddScoped(typeof(ICrudService<,>), typeof(CrudService<,>));
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
