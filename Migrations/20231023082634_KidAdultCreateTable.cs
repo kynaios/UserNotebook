1409aefea24118bab6e6c86de02b2e610fb7cc53
kynaios <blaise.witczak@gmail.com>
2023-10-24 08:31:44 +0200

    Create Kid and Adult migration

---
+++ Migrations/20231023082634_KidAdultCreateTable.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserNotebook.Migrations
+{
+    /// <inheritdoc />
+    public partial class KidAdultCreateTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "BagWeight",
+                table: "Users",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmployed",
+                table: "Users",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Salary",
+                table: "Users",
+                type: "real",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SchoolName",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BagWeight",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmployed",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Salary",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SchoolName",
+                table: "Users");
+        }
+    }
+}
