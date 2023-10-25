6186741b644b1df4f410c471f43e49b73be6763a
kynaios <blaise.witczak@gmail.com>
2023-10-25 12:30:55 +0200

    Update salary field to double migration

---
+++ Migrations/20231025095251_UpdateSalaryField.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserNotebook.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSalaryField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Salary",
+                table: "Users",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Salary",
+                table: "Users",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+        }
+    }
+}
