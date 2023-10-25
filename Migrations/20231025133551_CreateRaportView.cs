f4fdd4c9cbba5c7696faad23518e4dca3f8a093c
kynaios <blaise.witczak@gmail.com>
2023-10-25 16:25:48 +0200

    Create Raports migration

---
+++ Migrations/20231025133551_CreateRaportView.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserNotebook.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateRaportView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE OR REPLACE VIEW Raports AS
+            SELECT
+                ""Name"", ""Surname"", ""BirthDay"",
+                CASE
+                    WHEN ""Sex"" = 0 THEN 'Male'
+                    WHEN ""Sex"" = 1 THEN 'Female'
+                    ELSE ''
+                END AS Sex_Name,
+                CASE
+                    WHEN ""Sex"" = 1 AND EXTRACT(YEAR FROM age(""BirthDay"")) >= 18 THEN 'Ms'
+                    WHEN ""Sex"" = 1 THEN 'Miss'
+                    WHEN ""Sex"" = 0 THEN 'Mr'
+                    ELSE ''
+                END AS Title,
+                EXTRACT(YEAR FROM age(""BirthDay"")) AS Age
+            FROM
+                ""Users"" u ;
+        ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW IF EXISTS Raports;");
+        }
+    }
+}
