4e153b35461a4368429ba80b4fe331d03ac409f7
kynaios <blaise.witczak@gmail.com>
2023-10-25 14:32:04 +0200

    Display found user

---
+++ client/src/routes/[discriminator]/[id]/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+		import { Button } from '$lib/components/ui/button';
+	import * as Dialog from '$lib/components/ui/dialog';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+	export let data;
+
+	const {user} = data
+</script>
+
+<div class="grid gap-4 py-4">
+	<div class="grid grid-cols-4 items-center gap-4">
+		<Label class="text-right">Name</Label>
+		<span class="col-span-3">{user.name} </span>
+	</div>
+	<div class="grid grid-cols-4 items-center gap-4">
+		<Label class="text-right">Surname</Label>
+		<span class="col-span-3">{user.surname} </span>
+	</div>
+	<div class="grid grid-cols-4 items-center gap-4">
+		<Label class="text-right">Birthday</Label>
+		<span class="col-span-3">{user.birthDay} </span>
+	</div>
+	<div class="grid grid-cols-4 items-center gap-4">
+		<Label class="text-right">Sex</Label>
+		<span class="col-span-3">{user.sex === 0 ? "Male" : "Female"} </span>
+	</div>
+	{#if user.discriminator === 'Adult'}
+		<div class="grid grid-cols-4 items-center gap-4">
+			<Label
+				id="terms-label"
+				for="terms"
+				class="text-sm justify-self-end font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+			>
+				Employed?
+			</Label>
+			<span class="col-span-3">{user.isEmployed === true ? 'Yes' : No} </span>
+		</div>
+
+		{#if user.isEmployed}
+			<div class="grid grid-cols-4 items-center gap-4">
+				<Label class="text-right">Company Name</Label>
+				<span class="col-span-3">{user.companyName} </span>
+			</div>
+			<div class="grid grid-cols-4 items-center gap-4">
+				<Label class="text-right">Salary</Label>
+				<span class="col-span-3">{user.salary} </span>
+			</div>
+		{/if}
+	{:else if user.discriminator === 'Kid'}
+		<div class="grid grid-cols-4 items-center gap-4">
+			<Label class="text-right">School Name</Label>
+			<span class="col-span-3">{user.schoolName} </span>
+		</div>
+		<div class="grid grid-cols-4 items-center gap-4">
+			<Label class="text-right">Bag Weight</Label>
+			<span class="col-span-3">{user.bagWeight} </span>
+		</div>
+	{/if}
+</div>
