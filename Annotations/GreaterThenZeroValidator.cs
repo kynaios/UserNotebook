fcb411419888a3adda7663ea9c51331427d26024
kynaios <blaise.witczak@gmail.com>
2023-10-25 10:31:22 +0200

    Create GreaterThenZeroValidator and Birthday validators

---
+++ Annotations/GreaterThenZeroValidator.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UserNotebook.Annotations;
+
+public class GreaterThenZeroValidator : ValidationAttribute
+{
+    private const double Minimum = 0;
+    public double Maximum;
+
+    public GreaterThenZeroValidator(double Maximum)
+    {
+        this.Maximum = Maximum;
+    }
+
+    public override bool IsValid(object? value)
+    {
+        var requestValue = value as double?;
+        
+        return requestValue < Maximum && requestValue > Minimum;
+    }
+}
